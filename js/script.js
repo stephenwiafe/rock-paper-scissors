780e8e7343115f1944c0ed812c70523d44aa6d68
stephenwiafe <stephwiafe18@gmail.com>
2023-02-14 13:03:40 -0500

    Add four functions for game

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function getComputerChoice(computerChoice) {
-    if(computerChoice === 'Rock') {
-        computerChoice = 'Rock';
-    } else if(computerChoice === 'Paper') {
-        computerChoice = 'Paper';
-    } else {
-        computerChoice = 'Scissors';
-    }
-    return computerChoice;
+function getPlayerChoice() {
+
 }
 
-function playSingleRound(playerSelection, computerSelection) {
+function getComputerChoice(computerChoice) {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
 
 }
