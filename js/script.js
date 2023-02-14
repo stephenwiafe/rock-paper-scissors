abf928520d4a194619ce1eef13238540b815940e
stephenwiafe <stephwiafe18@gmail.com>
2023-02-13 23:55:31 -0500

    Add computerChoice and playSingleRound functions

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(computerChoice) {
+    if(computerChoice === 'Rock') {
+        computerChoice = 'Rock';
+    } else if(computerChoice === 'Paper') {
+        computerChoice = 'Paper';
+    } else {
+        computerChoice = 'Scissors';
+    }
+    return computerChoice;
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+
+}
