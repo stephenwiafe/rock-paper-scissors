ef34c4d5f977bd4e7eb4c681bbf8c41e4d87a83b
stephenwiafe <stephwiafe18@gmail.com>
2023-03-20 23:45:48 -0400

    Update function playRound

--- js/script.js
+++ js/script.js
@@ -14,43 +14,46 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
-  if((playerSelection === "rock" && computerSelection === "scissors") &&
-    (playerSelection < computerSelection)) {
+  if(playerSelection === "rock") {
+    if(computerSelection === "scissors"){
       playerScore++;
       return `You Win ${playerSelection} beats ${computerSelection}.`;
-  } else if((playerSelection === "rock" && computerSelection === "paper") &&
-    (playerSelection > computerSelection)) {
+    } else if(computerSelection === "paper"){
       computerScore++;
-      return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-  } else if((playerSelection === "paper" && computerSelection === "scissors") &&
-    (playerSelection <  computerSelection)) {
-      computerScore++;
-      return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-  } else if((playerSelection === "paper" && computerSelection === "rock") &&
-      (playerSelection < computerSelection)) {
-        playerScore++;
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else if((playerSelection === "scissors" && computerSelection === "rock") &&
-    (playerSelection > computerSelection)) {
-      computerScore++;
-      return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-  } else if((playerSelection === "scissors" && computerSelection === "paper") &&
-    (playerSelection > computerSelection)) {
-      playerScore++;
-      return `You Win! ${playerSelection} beats ${computerSelection}.`;
-  } else {
-    return `It's a tie! ${computerSelection} tied with ${playerSelection}.`;
+      return `You Lose ${computerSelection} beats ${playerSelection}.`
+    } else {
+      return `It's a tie! ${computerSelection} tied with ${playerSelection}.`
+    }
   }
-}
+  if(playerSelection === "paper") {
+    if(computerSelection === "scissors"){
+      computerScore++;
+      return `You Lose ${computerSelection} beats ${playerSelection}.`;
+    } else if(computerSelection === "rock"){
+      playerScore++;
+      return `You Win ${playerSelection} beats ${computerSelection}.`
+    } else {
+      return `It's a tie! ${playerSelection} tied with ${computerSelection}.`
+    }
+  }
+  if(playerSelection === "scissors") {
+    if(computerSelection === "rock"){
+      computerScore++;
+      return `You Lose ${computerSelection} beats ${playerSelection}.`;
+    } else if(computerSelection === "paper"){
+      playerScore++;
+      return `You Win ${playerSelection} beats ${computerSelection}.`
+    } else {
+      return `It's a tie! ${playerSelection} tied with ${computerSelection}.`
+    }
+  }
+} 
 
 //This is the main code that will be executed first
-
 function game() { 
-
+  
   for(let i = 0; i < 5; i++) {
     const playerChoice = prompt("Enter rock/paper/scissors:", "");
-
     console.log(playRound(playerChoice, getComputerChoice()));
     console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`);
   }
